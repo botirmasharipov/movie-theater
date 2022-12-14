6ca3831aad1dd89d14b415265cbd389b934a5cdc
botirmasharipov <masbotirjon963@gmail.com>
2022-12-14 11:30:39 -0500

    edit route endpoints

--- routes/show.js
+++ routes/show.js
@@ -18,21 +18,28 @@ router.get("/:id", async function (request, response) {
     response.json(shows)
 })
 
-// UPDATE the status of a show
-router.put('/:status', [check("status").not().trim().isEmpty().trim().isLength({ min: 5, max: 25 }).trim()],
+// GET shows of a particular genre (genre in req.params)
+router.get("/genres/:genre", async function (request, response) {
+    const shows = await Show.findByPk(request.params.genre)
+    response.json(shows)
+})
+
+// UPDATE the status of a show /shows/4/watched
+router.put('/:id/:status', [check("status").not().trim().isEmpty().trim().isLength({ min: 5, max: 25 }).trim()],
     async (req, res) => {
-        const findShow = await Show.findByPk(req.params.status)
+        const findShow = await Show.findByPk(req.params.id)
+        const findStatus = await Show.findByPk(req.params.status)
         await Show.update(req.body, {
             where:
             {
-                status: findShow.status
+                id: findShow.id
             }
         })
         res.json(await Show.findAll())
     })
 
 // Update the rating
-router.put('/:rating', [check("rating").not().trim().isEmpty().trim()], async (req, res) => {
+router.put('/id/:rating', [check("rating").not().trim().isEmpty().trim()], async (req, res) => {
     const findShow = await Show.findByPk(req.params.rating)
     await Show.update(req.body, {
         where:
--- routes/user.js
+++ routes/user.js
@@ -19,10 +19,15 @@ router.get("/:id", async function (request, response) {
 })
 
 // GET all shows watched by a user (user id in req.params)
+router.get("/:id/shows", async function (request, response) {
+    const user = await User.findByPk(request.params.id)
+    response.json(user)
+})
 
 // PUT update and add a show if a user has watched it
-router.put('/:id', async (req, res) => {
+router.put('/:id/shows/:showId', async (req, res) => {
     const findUser = await User.findByPk(req.params.id)
+    const findShow = await Show.findByPk(req.params.showId)
     await User.update(req.body, {
         where:
         {
